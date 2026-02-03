820adfe8d5ef84043b533b7f92c2314118c6e10e
Vishnu Mittal <vishnumittalacc026@gmail.com>
2026-02-03 16:33:54 +0530

    "another commit from ij"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("hello kundan from intellij ");
+        System.out.println("this another message from intellij");
     }
 }
