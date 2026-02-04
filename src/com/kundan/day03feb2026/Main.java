507d25e902e7888d184d808e10f98beacb3c347e
Vishnu Mittal <vishnumittalacc026@gmail.com>
2026-02-04 22:13:21 +0530

    "Remove duplicate element from array"

--- src/com/kundan/day03feb2026/Main.java
+++ src/com/kundan/day03feb2026/Main.java
@@ -1,20 +1,20 @@
-package com.kundan.day03feb2026;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-public class Main {
-    public static void main(String[] args){
-//        System.out.println("hello kundan from intellij ");
-//        System.out.println("this another message from intellij");
-        List<Integer> list = Arrays.asList(8,9,1,3,6,2,15,18);
-
-        List<Integer> finalResult = list.stream()
-                .filter(i ->i%2==0)
-                .collect(Collectors.toList());
-
-        System.out.println(finalResult);
-
-    }
-}
+//package com.kundan.day03feb2026;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//public class Main {
+//    public static void main(String[] args){
+////        System.out.println("hello kundan from intellij ");
+////        System.out.println("this another message from intellij");
+//        List<Integer> list = Arrays.asList(8,9,1,3,6,2,15,18);
+//
+//        List<Integer> finalResult = list.stream()
+//                .filter(i ->i%2==0)
+//                .collect(Collectors.toList());
+//
+//        System.out.println(finalResult);
+//
+//    }
+//}
