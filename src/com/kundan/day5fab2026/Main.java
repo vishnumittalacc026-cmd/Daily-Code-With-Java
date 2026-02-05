a01f8261ae9204d02297110fa83ef69806526a93
Vishnu Mittal <vishnumittalacc026@gmail.com>
2026-02-05 16:45:22 +0530

    "sorted and max value"

--- src/com/kundan/day5fab2026/Main.java
+++ src/com/kundan/day5fab2026/Main.java
@@ -14,26 +14,55 @@ public class Main {
          */
 
 
-        int[] arr={10,5,2,1,88,2,20,11,10};
+//        int[] arr={10,5,2,1,88,2,20,11,10};
+//
+//        int largest = Integer.MIN_VALUE;
+//        int secondLargest = Integer.MIN_VALUE;
+//
+////        System.out.println("largest="+largest);
+////        System.out.println("secondLargest="+secondLargest);
+//
+//        for (int num : arr) {
+//            if(num>largest) {
+//                secondLargest = largest;
+//                largest = num;
+//            } else if(num>secondLargest && num != largest) {
+//                secondLargest = num;
+//            }
+//        }
+//        if(secondLargest == Integer.MIN_VALUE) {
+//            System.out.println("no second largest element");
+//        } else {
+//            System.out.println("second largest element: " + secondLargest);
+//        }
 
-        int largest = Integer.MIN_VALUE;
-        int secondLargest = Integer.MIN_VALUE;
+//        //check if array is sorted
+//        int[] arr ={1,2,3,4,5,6,7,8,9};
+//
+//        boolean isSorted = true;
+//
+//        for(int i=0;i<arr.length-1;i++){
+//            if(arr[i] >arr[i+1]){
+//                isSorted =false;
+//                break;
+//            }
+//        }
+//
+//        System.out.println(isSorted);
 
-//        System.out.println("largest="+largest);
-//        System.out.println("secondLargest="+secondLargest);
+        // find the max element in the array
 
-        for (int num : arr) {
-            if(num>largest) {
-                secondLargest = largest;
-                largest = num;
-            } else if(num>secondLargest && num != largest) {
-                secondLargest = num;
-            }
-        }
-        if(secondLargest == Integer.MIN_VALUE) {
-            System.out.println("no second largest element");
-        } else {
-            System.out.println("second largest element: " + secondLargest);
-        }
+//        int[] arr={3,5,2,9,7,5,999};
+//
+//        int max =arr[0];
+//
+//        for(int num:arr){
+//            if(num>max){
+//                max = num;
+//            }
+//        }
+//        System.out.println("largest element in array is: "+max);
+
+
     }
 }
