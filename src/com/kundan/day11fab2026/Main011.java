fe3167ff6c248992c1e7393d52a8b980687930ea
kundan kumar <kundankumaracc20@gmail.com>
2026-02-11 18:43:17 +0530

    find the largest sequence in the string

---
+++ src/com/kundan/day11fab2026/Main011.java
@@ -0,0 +1,24 @@
+package com.kundan.day11fab2026;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main011 {
+    public static void main(String[] args){
+        String s ="abcabcbb";
+
+        Set<Character> set = new HashSet<>();
+
+        int left =0, maxLength=0;
+
+        for(int right =0;right<s.length();right++){
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLength =Math.max(maxLength,right-left+1);
+        }
+        System.out.println("Longest length: "+maxLength);
+    }
+}
