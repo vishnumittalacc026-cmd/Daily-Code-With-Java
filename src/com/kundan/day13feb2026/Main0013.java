0e7ca4a2294adb86d3f9a83f99681b30ed090283
kundan kumar <kundankumaracc20@gmail.com>
2026-02-13 22:03:54 +0530

    hello

--- src/com/kundan/day13feb2026/Main0013.java
+++ src/com/kundan/day13feb2026/Main0013.java
@@ -4,17 +4,33 @@ import java.util.*;
 
 public class Main0013 {
     public static void main(String[] args) {
-    String[] words = {"eat", "teat","tan","ate","nat","bat","hat"};
+//    String[] words = {"eat", "teat","tan","ate","nat","bat","hat"};
+//
+//        Map<String, List<String>>  map = new HashMap<>();
+//
+//        for(String word:words){
+//            char[] chars = word.toCharArray();
+//            Arrays.sort(chars);
+//            String key = new String(chars);
+//
+//            map.computeIfAbsent(key, k->new ArrayList<>()).add(word);
+//        }
+//        System.out.println(map.values());
 
-        Map<String, List<String>>  map = new HashMap<>();
+        // sort hashmap by value
 
-        for(String word:words){
-            char[] chars = word.toCharArray();
-            Arrays.sort(chars);
-            String key = new String(chars);
+        Map<String,Integer> map = new HashMap<>();
+        map.put("A",90);
+        map.put("B",1);
+        map.put("C",20);
+        map.put("D",4);
 
-            map.computeIfAbsent(key, k->new ArrayList<>()).add(word);
+        List<Map.Entry<String,Integer>> list = new ArrayList<>(map.entrySet());
+
+        list.sort(Map.Entry.comparingByValue());
+
+        for(Map.Entry<String,Integer> entry:list){
+            System.out.println(entry.getKey()+" "+entry.getValue());
         }
-        System.out.println(map.values());
     }
 }
