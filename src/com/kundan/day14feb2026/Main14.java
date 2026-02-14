0e1e311d409b692679c7cfa815ebdefb94fda631
kundan kumar <kundankumaracc20@gmail.com>
2026-02-14 21:07:44 +0530

    hello peter

--- src/com/kundan/day14feb2026/Main14.java
+++ src/com/kundan/day14feb2026/Main14.java
@@ -1,5 +1,6 @@
 package com.kundan.day14feb2026;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 public class Main14 {
@@ -8,10 +9,17 @@ public class Main14 {
         //takes one argument and return boolean
         // boolean test(T t)
 
-        Predicate<Integer> isEven = n->n%2 ==0;
+//        Predicate<Integer> isEven = n->n%2 ==0;
+//
+//        System.out.println(isEven.test(12));
+//        System.out.println(isEven.test(9));
 
-        System.out.println(isEven.test(12));
-        System.out.println(isEven.test(9));
+        // filter using predicate with stream
 
+        List<Integer> numbers = List.of(10,15,20,11,16,21);
+
+        numbers.stream()
+                .filter(n-> n>20)
+                .forEach(System.out::println);
     }
 }
