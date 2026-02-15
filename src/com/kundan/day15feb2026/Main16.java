d953a7bdf5c5741231fabc784a936c06256df0b0
kundan kumar <kundankumaracc20@gmail.com>
2026-02-15 21:02:58 +0530

    hello peter

--- src/com/kundan/day15feb2026/Main16.java
+++ src/com/kundan/day15feb2026/Main16.java
@@ -5,8 +5,14 @@ import java.util.List;
 
 public class Main16 {
     public static void main(String[] args){
-        List<Integer> list = Arrays.asList(10,11,12,15);
-
-        list.forEach(n ->System.out.println(n));
+//        List<Integer> list = Arrays.asList(10,11,12,15);
+//
+//        list.forEach(n ->System.out.println(n));
+// even numbers using lambda
+//        List<Integer> list =Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+//
+//        list.stream()
+//                .filter(n -> n%2 ==0)
+//                .forEach(System.out::println);
     }
 }
