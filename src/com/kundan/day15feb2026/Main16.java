176b32d485196ad3399b76f16db36bdffa5b9da7
kundan kumar <kundankumaracc20@gmail.com>
2026-02-15 20:53:48 +0530

    hello kundan

---
+++ src/com/kundan/day15feb2026/Main16.java
@@ -0,0 +1,12 @@
+package com.kundan.day15feb2026;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main16 {
+    public static void main(String[] args){
+        List<Integer> list = Arrays.asList(10,11,12,15);
+
+        list.forEach(n ->System.out.println(n));
+    }
+}
