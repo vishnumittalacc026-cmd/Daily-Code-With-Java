a75ebd327f72f8ff6d7f6f30d36c39266939bd3f
kundan kumar <kundankumaracc20@gmail.com>
2026-02-16 21:07:46 +0530

    selfstudyisgreatwork

--- src/com/kundan/day16fab2026/Main16.java
+++ src/com/kundan/day16fab2026/Main16.java
@@ -4,24 +4,38 @@ import java.util.HashMap;
 
 public class Main16 {
 
-    public static int firstUniChar(String s){
-        HashMap<Character,Integer> map = new HashMap<>();
+//    public static int firstUniChar(String s){
+//        HashMap<Character,Integer> map = new HashMap<>();
+//
+//        //counting frequency of each type of charter in the string
+//        for(int i=0; i<s.length(); i++){
+//            char ch = s.charAt(i);
+//            map.put(ch, map.getOrDefault(ch,0)+1);
+//        }
+//
+//        // finding first character with non frequency is one
+//        for(int i=0; i<s.length(); i++){
+//            if(map.get(s.charAt(i))==1){
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
 
-        //counting frequency of each type of charter in the string
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch,0)+1);
+    public static int firstUniChar(String s){
+        int[] freq = new int[26];
+
+        for(char ch:s.toCharArray()){
+            freq[ch - 'a']++;
         }
 
-        // finding first character with non frequency is one
-        for(int i=0; i<s.length(); i++){
-            if(map.get(s.charAt(i))==1){
+        for (int i=0;i<s.length();i++){
+            if(freq[s.charAt(i)-'a']==1){
                 return i;
             }
         }
         return -1;
     }
-
     public static void main(String[] args){
         String s = "loveleetcode";
 
