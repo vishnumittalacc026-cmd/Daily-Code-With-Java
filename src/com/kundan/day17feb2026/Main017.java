3d8a2d691406ad8638f791e8346e09b97815d221
kundan kumar <kundankumaracc20@gmail.com>
2026-02-17 15:31:22 +0530

    kd..

--- src/com/kundan/day17feb2026/Main017.java
+++ src/com/kundan/day17feb2026/Main017.java
@@ -4,31 +4,50 @@ import java.util.*;
 
 public class Main017 {
     public static void main(String[] args) {
-        int[] nums = {1, 1, 1, 2, 2, 3};
+        int[] nums = {1, 1, 1, 2, 2,2,2,3,3,3,3,3,3};
         int k = 2;
         System.out.println(topKFrequent(nums, k));
     }
 
-    public static List<Integer> topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+//    public static List<Integer> topKFrequent(int[] nums, int k) {
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//
+//        //step 1: count frequency
+//        for (int num : nums) {
+//            map.put(num, map.getOrDefault(num, 0) + 1);
+//        }
+//        //step 2: convert map to list
+//        List<Map.Entry<Integer, Integer>> list =
+//                new ArrayList<>(map.entrySet());
+//        //System.out.println(list);
+//        //sort by frequency
+//        Collections.sort(list, (a, b) ->
+//                b.getValue() - a.getValue());
+//        //collect top k elements
+//        List<Integer> result = new ArrayList<>();
+//
+//        for (int i = 0; i < k; i++) {
+//            result.add(list.get(i).getKey());
+//        }
+//        return result;
+//    }
 
-        //step 1: count frequency
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+  public static List<Integer> topKFrequent(int[] nums, int k){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
-        //step 2: convert map to list
-        List<Map.Entry<Integer, Integer>> list =
-                new ArrayList<>(map.entrySet());
-        //System.out.println(list);
-        //sort by frequency
-        Collections.sort(list, (a, b) ->
-                b.getValue() - a.getValue());
-        //collect top k elements
-        List<Integer> result = new ArrayList<>();
+        // Min haep based on frequency
+      PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) ->
+              map.get(a)-map.get(b));
 
-        for (int i = 0; i < k; i++) {
-            result.add(list.get(i).getKey());
+        for(int key: map.keySet()){
+            pq.add(key);
+
+            if(pq.size()>k){
+                pq.poll();
+            }
         }
-        return result;
-    }
+        return new ArrayList<>(pq);
+  }
 }
