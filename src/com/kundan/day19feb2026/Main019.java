5b7dac948390c79c87afa12162b648e12ba3ddee
kundan kumar <kundankumaracc20@gmail.com>
2026-02-19 23:29:43 +0530

    calling a non-static to static main method

--- src/com/kundan/day19feb2026/Main019.java
+++ src/com/kundan/day19feb2026/Main019.java
@@ -1,18 +1,29 @@
 package com.kundan.day19feb2026;
 
+import com.kundan.day5fab2026.Main;
+
 public class Main019 {
-    private String message ="hello from outer class";
+//    private String message ="hello from outer class";
+//
+//    class  InnerClass {
+//        void display(){
+//            System.out.println(message);
+//        }
+//    }
+      public String nonStaticMethod(){
+          return "returning from non-static method to static main method";
+      }
 
-    class  InnerClass {
-        void display(){
-            System.out.println(message);
-        }
-    }
     public static void main(String[] args){
-        Main019 outer = new Main019();
+//        Main019 outer = new Main019();
+//
+//        Main019.InnerClass inner= outer.new InnerClass();
+//        inner.display();
 
-        Main019.InnerClass inner= outer.new InnerClass();
-        inner.display();
+        // calling a non static method in the static main method.
 
+        Main019 ob = new Main019();
+
+        System.out.println(ob.nonStaticMethod());
     }
 }
