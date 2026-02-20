bee3912edc22e3a064293f5a0b91be0528d7549d
kundan kumar <kundankumaracc20@gmail.com>
2026-02-21 00:01:51 +0530

    kd

--- src/com/kundan/day20feb2026/Main020.java
+++ src/com/kundan/day20feb2026/Main020.java
@@ -1,32 +1,44 @@
 package com.kundan.day20feb2026;
 
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main020 {
 
-public static char findFirstNonReaption(String input){
-    Optional<Character> result = input.chars()
-            .mapToObj(c->(char)c)
-            .collect(Collectors.groupingBy(
-                            Function.identity(),
-                            LinkedHashMap::new,
-                            Collectors.counting()
-            ))
-            .entrySet()
-            .stream()
-            .filter(entry -> entry.getValue()==1)
-            .map(Map.Entry::getKey)
-            .findFirst();
-
-    return result.orElse('-');
-}
+//public static char findFirstNonReaption(String input){
+//    Optional<Character> result = input.chars()
+//            .mapToObj(c->(char)c)
+//            .collect(Collectors.groupingBy(
+//                            Function.identity(),
+//                            LinkedHashMap::new,
+//                            Collectors.counting()
+//            ))
+//            .entrySet()
+//            .stream()
+//            .filter(entry -> entry.getValue()==1)
+//            .map(Map.Entry::getKey)
+//            .findFirst();
+//
+//    return result.orElse('-');
+//}
     public static void main(String[] args){
-        String input = "aabcbcdede";
-        System.out.println(findFirstNonReaption(input));
+//        String input = "aabcbcdede";
+//        System.out.println(findFirstNonReaption(input));
+
+        int[] arr = {1,2,1,2,4,2,6};
+
+        Set<Integer> duplicates = Arrays.stream(arr)
+                .boxed()
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        Collectors.counting()
+                ))
+                .entrySet()
+                .stream()
+                .filter(entry ->entry.getValue()>1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
+        System.out.println(duplicates);
     }
 }
