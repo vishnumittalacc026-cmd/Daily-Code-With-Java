687282a721588f2812d643f224bfbf11dabdabfb
kundan kumar <kundankumaracc20@gmail.com>
2026-02-22 00:08:24 +0530

    multithreading

--- src/com/kundan/day21feb2026/Main021.java
+++ src/com/kundan/day21feb2026/Main021.java
@@ -3,5 +3,6 @@ package com.kundan.day21feb2026;
 public class Main021 {
     public static void main(String[] args){
         System.out.println("hello world ");
+
     }
 }
---
+++ src/com/kundan/day22feb2026/Main026.java
@@ -0,0 +1,21 @@
+package com.kundan.day22feb2026;
+
+public class Main026 {
+    public static void main(String[] args){
+        Thread t1 =new Thread(()->System.out.println("thread t1 is running"));
+        Thread t2 =new Thread(()->System.out.println("thread t2 is running"));
+        Thread t3 =new Thread(()->System.out.println("thread t3 is running"));
+
+        try {
+            t1.start();
+            t1.join();
+            t2.start();
+            t2.join();
+            t3.start();
+            t3.join();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+    }
+}
