2c08631f0bfb8196ac0440d05c80c7553b92a269
kundan kumar <kundankumaracc20@gmail.com>
2026-02-24 08:59:17 +0530

    Feb 24 8:59

--- src/com/kundan/day24feb2026/Main024.java
+++ src/com/kundan/day24feb2026/Main024.java
@@ -1,7 +1,9 @@
 package com.kundan.day24feb2026;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Main024 {
@@ -10,8 +12,16 @@ public class Main024 {
         List<Integer> list1 = Arrays.asList(1,2,3,4,5);
         List<Integer> list2 = Arrays.asList(3,4,5,6,7);
 
+//        List<Integer> common = list1.stream()
+//                .filter(list2::contains)
+//                .collect(Collectors.toList());
+//        System.out.println(common);
+
+        //optimized way
+        Set<Integer> set = new HashSet<>(list2);
+
         List<Integer> common = list1.stream()
-                .filter(list2::contains)
+                .filter(set::contains)
                 .collect(Collectors.toList());
         System.out.println(common);
     }
