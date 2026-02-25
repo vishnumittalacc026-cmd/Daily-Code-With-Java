bea7ecf6a688461cd7255a86f589b98e7d4a027e
kundan kumar <kundankumaracc20@gmail.com>
2026-02-25 11:41:55 +0530

    Feb:25::2026:10:41:AM

--- src/com/kundan/day25feb2026/Main025.java
+++ src/com/kundan/day25feb2026/Main025.java
@@ -4,6 +4,9 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main025 {
+    enum Category{
+        EVEN,ODD,DIVISIBLE_BY_3
+    }
     public static void main(String[] args){
         //Start Time
         long startTime = System.nanoTime();
@@ -55,24 +58,49 @@ public class Main025 {
 //                }));
 //        // Time taken in millisecond: 10ms
 
-        //5th approach
-        Map<String,List<Integer>> result = new HashMap<>();
-        result.put("Even:",new ArrayList<>());
-        result.put("Odd:",new ArrayList<>());
-        result.put("Divisible by 3:",new ArrayList<>());
+//        //5th approach
+//        Map<String,List<Integer>> result = new HashMap<>();
+//        result.put("Even:",new ArrayList<>());
+//        result.put("Odd:",new ArrayList<>());
+//        result.put("Divisible by 3:",new ArrayList<>());
+//
+//        for(Integer n:list){
+//            if(n%2==0){
+//                result.get("Even:").add(n);
+//            }
+//            if(n%2 !=0){
+//                result.get("Odd:").add(n);
+//            }
+//            if(n%3==0){
+//                result.get("Divisible by 3:").add(n);
+//            }
+//        }
+//        //Time taken in millisecond: 0ms
 
+        //Optimized approach
+        //Pre-initialize map with empty lists
+        Map<Category,List<Integer>> result = new EnumMap<>(Category.class);
+        for(Category c : Category.values()){
+            result.put(c,new ArrayList<>());
+        }
+
+        //Classify numbers int all applicable categories
         for(Integer n:list){
-            if(n%2==0){
-                result.get("Even:").add(n);
-            }
-            if(n%2 !=0){
-                result.get("Odd:").add(n);
+//            if(n%2==0){
+//                result.get(Category.EVEN).add(n);
+//            } else {
+//                result.get(Category.ODD).add(n);
+//            }
+            //check even/odd using bitwise AND
+            if((n & 1)==0){
+                result.get(Category.EVEN).add(n);
+            } else {
+                result.get(Category.ODD).add(n);
             }
             if(n%3==0){
-                result.get("Divisible by 3:").add(n);
+                result.get(Category.DIVISIBLE_BY_3).add(n);
             }
         }
-        //Time taken in millisecond: 0ms
 
         //End time
         long endTime = System.nanoTime();
