ed354b48b33ae890c434de8f303fc490d0540960
kundan kumar <kundankumaracc20@gmail.com>
2026-02-28 11:34:41 +0530

    Feb:28::2026:11:34:AM

--- src/com/kundan/day28feb2026/Main028.java
+++ src/com/kundan/day28feb2026/Main028.java
@@ -33,18 +33,31 @@ public class Main028 {
 //                .toString();
 //        System.out.println(reversed_word);
 
-        char[] chars =new char[10_000_000];
-        java.util.Arrays.fill(chars,'A');
+//        char[] chars =new char[100_000_000];
+//        java.util.Arrays.fill(chars,'A');
+//        String word = new String(chars);
+//
+//        String reversedWord = new StringBuilder(word)
+//                .reverse()
+//                        .toString();
+//
+//        System.out.println("word length: "+word.length());
+//        System.out.println(reversedWord);
+//        //Total time: 262 ms for one million character
+        //Total time: 1223 ms for ten million character
+
+        char[] chars = new char[100_000_000];
+        java.util.Arrays.fill(chars,'k');
         String word = new String(chars);
+        StringBuilder reversedWord = new StringBuilder(word);
 
-        String reversedWord = new StringBuilder(word)
-                .reverse()
-                        .toString();
-
-        System.out.println("word length: "+word.length());
-        System.out.println(reversedWord);
-        //Total time: 262 ms
-
+        StringBuilder reversedFinalWord = new StringBuilder("");
+        for (int i = reversedWord.length() - 1; i >= 0; i--){
+            reversedFinalWord.append(reversedWord.charAt(i));
+        }
+        System.out.println(reversedFinalWord);
+//        //Total time: 260 ms for one million character
+//        //Total time: 2107 ms for 10 million character
         //end_timer
         long endTimer = System.nanoTime();
 
