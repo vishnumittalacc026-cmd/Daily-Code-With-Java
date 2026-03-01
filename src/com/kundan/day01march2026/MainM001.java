32794966d38288eb1ca32b685110002c63f8eb71
kundan kumar <kundankumaracc20@gmail.com>
2026-03-01 12:15:01 +0530

    March:1::2026::12:15:PM

--- src/com/kundan/day01march2026/MainM001.java
+++ src/com/kundan/day01march2026/MainM001.java
@@ -1,5 +1,6 @@
 package com.kundan.day01march2026;
 
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -7,13 +8,33 @@ public class MainM001 {
     public static void main(String[] args){
         //Start timer
         long startTimer = System.nanoTime();
-        String input = "java8";
+//        String input = "java8";
+        int length =100_000_000;
+        Random random = new Random();
+        StringBuilder sb= new StringBuilder(length);
 
+        //Generating random characters
+        for(int i=0;i<length;i++){
+            int choice = random.nextInt(3);
+            char c;
+            if(choice ==0){
+                c=(char) ('A'+ random.nextInt(26));
+            } else if (choice ==1) {
+                c =(char) ('a'+random.nextInt(26));
+            } else {
+                c =(char)('0'+ random.nextInt(10));
+            }
+            sb.append(c);
+        }
+        String input = sb.toString();
         String reversed = IntStream.rangeClosed(1,input.length())
                 .mapToObj(i->input.charAt(input.length()-i))
                 .map(String::valueOf)
                 .collect(Collectors.joining());
         System.out.println(reversed);
+        //total time for program execution: 283 for 1_000_000 character
+        //total time for program execution: 732 for 10_000_000 character
+        //total time for program execution: 5486 for 100_000_000 character
 
         //end timer
         long endTimer = System.nanoTime();
