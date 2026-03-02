88de6f769c930ccc80a949e62c12cffa17a8e4d3
kundan kumar <kundankumaracc20@gmail.com>
2026-03-02 11:32:43 +0530

    March:2::2026::11:32:AM

---
+++ src/com/kundan/day02march2026/MainM002.java
@@ -0,0 +1,29 @@
+package com.kundan.day02march2026;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class MainM002 {
+    public static void main(String[] args){
+        //start timer
+        long startTimer = System.nanoTime();
+
+        //Find Duplicate Elements in List.
+        List<Integer> list = Arrays.asList(1,2,3,4,5,4,5,6,7,2,9,1);
+
+        Set<Integer> duplicates = list.stream()
+                .filter(n -> Collections.frequency(list,n)>1)
+                .collect(Collectors.toSet());
+        System.out.println(duplicates);
+        //end timer
+        long endTimer = System.nanoTime();
+
+        //execution time
+        long executionTime = (endTimer-startTimer)/1_000_000;
+
+        System.out.println("Execution time for program: "+executionTime);
+    }
+}
