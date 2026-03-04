2804c36478a9bb662e42d3c243683bb44490ce58
kundan kumar <kundankumaracc20@gmail.com>
2026-03-04 09:47:25 +0530

    Mar:4::2026::09:48:AM

--- src/com/kundan/day04march2026/MainM004.java
+++ src/com/kundan/day04march2026/MainM004.java
@@ -1,6 +1,7 @@
 package com.kundan.day04march2026;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -9,18 +10,34 @@ public class MainM004 {
         // Start timer
         long startTimer = System.nanoTime();
         List<Integer> numbers = Arrays.asList(1,8,2,11,8,15,12,10,0,17);
-        Integer secondHightestNumber = numbers.stream()
-                .sorted(Comparator.reverseOrder())
-                .skip(1)
-                .findFirst()
-                .orElse(null);
-        System.out.println("Second highest number is: "+secondHightestNumber);
-        //Program time: 111
+//        Integer secondHightestNumber = numbers.stream()
+//                .sorted(Comparator.reverseOrder())
+//                //.sorted((a,b)-> b.compareTo(a))
+//                .skip(1)
+//                .findFirst()
+//                .orElse(null);
+//        System.out.println("Second highest number is: "+secondHightestNumber);
+//        //Program time: 4 ms when Comparator.reverseOrder() used
+//        //Program time: 5 ms when lambda method used
+
+        Collections.sort(numbers, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return b.compareTo(a);
+            }
+        });
+
+        Integer secondHighestNumber = null;
+        if(numbers.size()>1){
+            secondHighestNumber = numbers.get(1);
+        }
+        System.out.println(secondHighestNumber);
+
         //End timer
         long endTimer = System.nanoTime();
 
         //Program time
         long programTime = (endTimer-startTimer)/1_000_000;
-        System.out.println("Program time: "+programTime);
+        System.out.println("Program time: "+programTime+" ms");
     }
 }
