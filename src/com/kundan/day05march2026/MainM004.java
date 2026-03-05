78b48048c4901fc39ad0b135fc3ca3e38e49bbcd
kundan kumar <kundankumaracc20@gmail.com>
2026-03-05 10:21:06 +0530

    March:05::2026::10:21:AM

--- src/com/kundan/day05march2026/MainM004.java
+++ src/com/kundan/day05march2026/MainM004.java
@@ -1,8 +1,6 @@
 package com.kundan.day05march2026;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class MainM004 {
@@ -14,17 +12,41 @@ public class MainM004 {
         map.put("A",3);
         map.put("B",1);
         map.put("C",2);
+        map.put("H",5);
+        map.put("G",9);
 
-        Map<String,Integer> sorted = map.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByValue())
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (e1,e2) ->e1,
-                        LinkedHashMap::new
-                ));
-        System.out.println(sorted);
+//        Map<String,Integer> sorted = map.entrySet()
+//                        .stream()
+//                        .sorted(Map.Entry.comparingByValue())
+//                        .collect(Collectors.toMap(
+//                                Map.Entry::getKey,
+//                                Map.Entry::getValue,
+//                                (e1,e2)->e1,
+//                                LinkedHashMap::new
+//                        ));
+//        System.out.println(sorted);
+//        //Program Time: 18 millisecond for small data set
+
+        //second approach by using loop
+
+        //convert map to list
+        List<Map.Entry<String,Integer>> entryList = new ArrayList<>(map.entrySet());
+        //sort
+        Collections.sort(entryList, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
+                return e1.getValue().compareTo(e2.getValue());
+            }
+        });
+
+        //insert sorted entries to linked hashmap
+        Map<String,Integer> sortedMap = new LinkedHashMap<>();
+        for(Map.Entry<String,Integer> entry:entryList){
+            sortedMap.put(entry.getKey(),entry.getValue());
+        }
+        System.out.println(sortedMap);
+        //Program Time: 2 millisecond for small data set
+
 
         //end timer
         long endTimer =System.nanoTime();
