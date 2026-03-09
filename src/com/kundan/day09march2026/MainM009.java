5df18906401602379847d19b63cd1a9f24cc6f1a
kundan kumar <kundankumaracc20@gmail.com>
2026-03-09 10:40:10 +0530

    March:09::2026::10:40:AM

--- src/com/kundan/day09march2026/MainM009.java
+++ src/com/kundan/day09march2026/MainM009.java
@@ -1,10 +1,25 @@
 package com.kundan.day09march2026;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class MainM009 {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
-        System.out.println("hello kundan");
+//        System.out.println("hello kundan");
+
+        String sentence = "java is java and java is powerful";
+
+        Map<String, Long> wordCount =
+                Arrays.stream(sentence.split(" "))
+                        .collect(Collectors.groupingBy(
+                                Function.identity(),
+                                Collectors.counting()));
+
+        System.out.println(wordCount);
         //end timer
         long endTimer = System.nanoTime();
 
