ff2633ea6969037a93ee443c191abd56f1c26e0a
kundan kumar <kundankumaracc20@gmail.com>
2026-03-12 11:33:04 +0530

    March:12::2026::11:33:AM

--- src/com/kundan/day12march2026/Application001.java
+++ src/com/kundan/day12march2026/Application001.java
@@ -1,18 +1,25 @@
 package com.kundan.day12march2026;
 
 public class Application001 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         //Start Timer
         long startTimer = System.nanoTime();
-        //Creating virtual thread using Thread.startVirtualThread()
-        Thread virtualThread =Thread.startVirtualThread(()->{
-            System.out.println("Running in virtual thread: "+Thread.currentThread());
-        });
-        try {
-            virtualThread.join();
-        } catch (InterruptedException e){
-            e.printStackTrace();
-        }
+//        //Creating virtual thread using Thread.startVirtualThread()
+//        Thread virtualThread =Thread.startVirtualThread(()->{
+//            System.out.println("Running in virtual thread: "+Thread.currentThread());
+//        });
+//        try {
+//            virtualThread.join();
+//        } catch (InterruptedException e){
+//            e.printStackTrace();
+//        }
+        //Creating Virtual Thread using Thread.ofVirtual()
+        Thread thread = Thread.ofVirtual()
+                .name("My-Virtual-Thread")
+                .start(()->{
+                    System.out.println(Thread.currentThread());
+                });
+        thread.join();
 
         //End Timer
         long endTimer = System.nanoTime();
