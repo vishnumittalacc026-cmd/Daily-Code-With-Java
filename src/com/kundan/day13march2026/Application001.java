1334243dfdcf88cf152bff623f1feb76b6225495
kundan kumar <kundankumaracc20@gmail.com>
2026-03-13 09:11:03 +0530

    March:13::2026::09:11:AM

---
+++ src/com/kundan/day13march2026/Application001.java
@@ -0,0 +1,10 @@
+package com.kundan.day13march2026;
+
+public class Application001 {
+    public static void main(String[] args){
+        User user = new User(1,"Vishnu","vishnu@gmail.com");
+
+        System.out.println(user.name());
+        System.out.println(user);
+    }
+}
---
+++ src/com/kundan/day13march2026/User.java
@@ -0,0 +1,3 @@
+package com.kundan.day13march2026;
+
+public record User (int id,String name,String email){ }
