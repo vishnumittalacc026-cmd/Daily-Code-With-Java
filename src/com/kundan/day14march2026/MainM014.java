7ceaa06f51fbb7a2c11117aa801a3ee45a6e1e81
kundan kumar <kundankumaracc20@gmail.com>
2026-03-14 22:25:01 +0530

    March:14::2026::22:25:PM

--- src/com/kundan/day14march2026/MainM014.java
+++ src/com/kundan/day14march2026/MainM014.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 
 public class MainM014 {
     public static void main(String[] args){
+        //start timer
+        long startTimer = System.nanoTime();
         String sentence = "java is good java is powerful programming language";
 
         Map<String,Long> map =
@@ -16,5 +18,11 @@ public class MainM014 {
                                 Collectors.counting()
                         ));
         System.out.println(map);
+
+        //end timer
+        long endTimer =System.nanoTime();
+        //program time
+        long programTime = (endTimer-startTimer)/1_000_000;
+        System.out.println("Program Time: "+programTime+" ms");
     }
 }
