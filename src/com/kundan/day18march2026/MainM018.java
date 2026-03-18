f7c63b7071190106a9bdb9158d32ce5eed16bdc6
kundan kumar <kundankumaracc20@gmail.com>
2026-03-18 11:15:02 +0530

    March:18::2026::11:15:AM

--- src/com/kundan/day18march2026/MainM018.java
+++ src/com/kundan/day18march2026/MainM018.java
@@ -24,10 +24,32 @@ public class MainM018 {
 
         //End Timer
         long endTimer = System.nanoTime();
-        System.out.println(lengthOfLongestSubstring("abcdababcdeaaa"));
+//        System.out.println(lengthOfLongestSubstring("abcdababcdeaaa"));
+        String result = longestSubstring("abcabcbb");
+        System.out.println("Longest substring: "+result);
+        System.out.println("Length: "+result.length());
+
         //Program Time
         long programTime =(endTimer-startTimer)/1_000_000;
 
         System.out.println("Program Time: "+programTime+" millisecond");
+    }
+
+    public static String longestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int left =0,max =0, start=0;
+        for(int right =0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+
+            if (right - left + 1 > max) {
+                max = right - left + 1;
+                start = left;
+            }
+        }
+        return s.substring(start, start + max);
     }
 }
