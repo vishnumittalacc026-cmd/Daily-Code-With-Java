0416ac11a4d696a9c2a907fdaab6bc2c8fba5bf2
kundan kumar <kundankumaracc20@gmail.com>
2026-03-25 09:52:01 +0530

    March:25::2026::09:52:AM

---
+++ src/com/kundan/day25march2026/MainM025.java
@@ -0,0 +1,28 @@
+package com.kundan.day25march2026;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainM025 {
+    public static void main(String[] args){
+        //start timer
+        long startTimer = System.nanoTime();
+        //creating a list of integer
+        List<Integer> list = Arrays.asList(1,12,11,13,14,15,17);
+
+        //stream the list, convert to int, calculate average
+        double avg =
+                list.stream()
+                        .mapToInt(Integer::intValue)
+                        .average()
+                        .orElse(0);
+        System.out.printf("Average: %.2f%n",avg);
+        //end timer
+        long endTimer = System.nanoTime();
+
+        //program time
+        long programTime = (endTimer-startTimer)/1_000_000;
+
+        System.out.println("Program Time: "+programTime+" ms");
+    }
+}
