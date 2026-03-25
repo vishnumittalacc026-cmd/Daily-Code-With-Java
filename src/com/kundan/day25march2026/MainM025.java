23a1d8add9ed3e158eb029593fae15012d97899b
kundan kumar <kundankumaracc20@gmail.com>
2026-03-25 10:13:02 +0530

    March:25::2026::10:13:AM

--- src/com/kundan/day25march2026/MainM025.java
+++ src/com/kundan/day25march2026/MainM025.java
@@ -2,21 +2,33 @@ package com.kundan.day25march2026;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class MainM025 {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
         //creating a list of integer
-        List<Integer> list = Arrays.asList(1,12,11,13,14,15,17);
+//        List<Integer> list = Arrays.asList(1,12,11,13,14,15,17);
+//
+//        //stream the list, convert to int, calculate average
+//        double avg =
+//                list.stream()
+//                        .mapToInt(Integer::intValue)
+//                        .average()
+//                        .orElse(0);
+//        System.out.printf("Average: %.2f%n",avg);
 
-        //stream the list, convert to int, calculate average
-        double avg =
-                list.stream()
-                        .mapToInt(Integer::intValue)
-                        .average()
-                        .orElse(0);
-        System.out.printf("Average: %.2f%n",avg);
+        //using parallel stream for small dataset
+        List<Integer> smallList = IntStream.rangeClosed(1,1000).boxed().toList();
+
+        double avgSmallParallel =smallList
+                .parallelStream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0);
+        System.out.println("Smalldataset parallelstream: "+avgSmallParallel);
+        //Program Time: 34 ms for small dataset
         //end timer
         long endTimer = System.nanoTime();
 
