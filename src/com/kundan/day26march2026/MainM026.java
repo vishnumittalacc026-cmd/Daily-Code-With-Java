a5e40fa1795785f7994a939373b0b1fd395266dc
kundan kumar <kundankumaracc20@gmail.com>
2026-03-26 09:44:01 +0530

    March:26::2026::09:44:AM

--- src/com/kundan/day26march2026/MainM026.java
+++ src/com/kundan/day26march2026/MainM026.java
@@ -1,5 +1,8 @@
 package com.kundan.day26march2026;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class MainM026 {
     public static int maxProfit(int[] prices){
         int min = Integer.MAX_VALUE;
@@ -11,15 +14,31 @@ public class MainM026 {
         }
         return profit;
     }
+
+    //contains duplicate
+    public static boolean containsDuplicate(int[] nums){
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(!set.add(num))
+                return true;
+        }
+        return false;
+    }
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
-        int[] prices1 = {7,1,5,3,6,4};
-        int[] prices2 = {7,6,4,3,1};
+//        int[] prices1 = {7,1,5,3,6,4};
+//        int[] prices2 = {7,6,4,3,1};
+//
+//        System.out.println("Max Profit (prices1): "+maxProfit(prices1));
+//        System.out.println("Max Profit (prices2): "+maxProfit(prices2));
 
-        System.out.println("Max Profit (prices1): "+maxProfit(prices1));
-        System.out.println("Max Profit (prices2): "+maxProfit(prices2));
-        //end timer
+        int[] num1 = {1,2,3,4};
+        int[] num2 = {1,2,3,1};
+
+        System.out.println(containsDuplicate(num1));
+        System.out.println(containsDuplicate(num2));
+//        //end timer
         long endTimer = System.nanoTime();
         //program time
         long programTime = (endTimer-startTimer)/1_000_000;
