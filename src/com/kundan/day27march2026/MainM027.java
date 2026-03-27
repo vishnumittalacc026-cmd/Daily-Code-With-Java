93471ab85aa43dfe9742b030d014f113ca7ea444
kundan kumar <kundankumaracc20@gmail.com>
2026-03-27 09:29:04 +0530

    March:27::2026::09:29:AM

--- src/com/kundan/day27march2026/MainM027.java
+++ src/com/kundan/day27march2026/MainM027.java
@@ -1,21 +1,37 @@
 package com.kundan.day27march2026;
 
-public class MainM027 {
-    public static int maxSubArray(int[] nums){
-        int max = nums[0], current = nums[0];
+import java.util.Arrays;
 
-        for(int i=0; i<nums.length; i++){
-            current = Math.max(nums[i], current+nums[i]);
-            max = Math.max(max, current);
+public class MainM027 {
+    public static int[] maxSubArray(int[] nums){
+        int max = nums[0], current =nums[0];
+        int start =0, end =0, tempStart =0;
+
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]> current+nums[i]){
+                current=nums[i];
+                tempStart=i;
+            } else {
+                current += nums[i];
+            }
+            //update max if we found a better sum
+            if(current > max){
+                max =current;
+                start = tempStart;
+                end=i;
+            }
         }
-        return max;
+        return Arrays.copyOfRange(nums,start,end+1);
     }
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
         // Maximum subarray(Kadane's)
         int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
-        System.out.println("Maximum Subarray Sum: "+maxSubArray(nums));
+        int[] result = maxSubArray(nums);
+
+        System.out.println("Maximum Subarray: "+ Arrays.toString(result));
+        System.out.println("Maximum Sum: "+Arrays.stream(result).sum());
         //end timer
         long endTimer = System.nanoTime();
 
