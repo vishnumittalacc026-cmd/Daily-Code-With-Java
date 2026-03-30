d50dfaf3638da015f699f030695b575f980ba145
kundan kumar <kundankumaracc20@gmail.com>
2026-03-30 09:57:02 +0530

    March:30::2026::09:57:AM

--- src/com/kundan/day30march2026/MainM030.java
+++ src/com/kundan/day30march2026/MainM030.java
@@ -1,10 +1,23 @@
 package com.kundan.day30march2026;
 
 public class MainM030 {
+    public static String formatter(Object o){
+        return switch (o){
+            case Integer i ->"Integer: "+i;
+            case String s ->"String: " +s;
+            case Double d -> "Double: "+d;
+            case null -> "Null value";
+            default -> "unknown type: "+o.getClass().getSimpleName();
+        };
+    }
     public static void main(String[] args){
         //Strat Timer
         long startTimer = System.nanoTime();
-
+        System.out.println(formatter(42));
+        System.out.println(formatter("hello"));
+        System.out.println(formatter(3.14));
+        System.out.println(formatter(null));
+        System.out.println(formatter(true));
         //End Timer
         long endTimer = System.nanoTime();
 
