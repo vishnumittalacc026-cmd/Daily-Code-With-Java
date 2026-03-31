3a0bbf0b6938bece82bd66922588ddd98c802e4c
kundan kumar <kundankumaracc20@gmail.com>
2026-03-31 08:42:16 +0530

    March:31::2026::08:42:AM

--- src/com/kundan/day31march2026/MainM031.java
+++ src/com/kundan/day31march2026/MainM031.java
@@ -1,10 +1,23 @@
 package com.kundan.day31march2026;
 
+import java.util.random.RandomGenerator;
+
 public class MainM031 {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        //get the default random generator
+        RandomGenerator generator = RandomGenerator.getDefault();
 
+        //Generate a random integer
+        int number = generator.nextInt();
+
+        //print the random number
+        System.out.println("Random number: "+number);
+
+        //you can also generate numbers within a range
+        int boundedNumber = generator.nextInt(100);
+        System.out.println("Random number between 0 and 99: "+boundedNumber);
         //end timer
         long endTimer = System.nanoTime();
 
