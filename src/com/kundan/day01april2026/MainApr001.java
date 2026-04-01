4e8ea148410adcbfc53e844ccc990e19c063fa73
kundan kumar <kundankumaracc20@gmail.com>
2026-04-01 09:48:09 +0530

    April:01::2026::09:48:AM

--- src/com/kundan/day01april2026/MainApr001.java
+++ src/com/kundan/day01april2026/MainApr001.java
@@ -1,9 +1,30 @@
 package com.kundan.day01april2026;
 
 public class MainApr001 {
+    public boolean isAnagram(String s, String t){
+        if(s.length() != t.length())
+            return false;
+
+        int[] count = new int[26];
+        for (char c: s.toCharArray())
+            count[c - 'a']++;
+        for (char c : t.toCharArray())
+            count[c-'a']--;
+
+        for(int c:count){
+            if(c != 0)
+                return false;
+        }
+        return true;
+    }
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        MainApr001 checker = new MainApr001();
+
+        System.out.println(checker.isAnagram("listen","silent"));
+        System.out.println(checker.isAnagram("triangle","integral"));
+        System.out.println(checker.isAnagram("apple","pale"));
 
         //end timer
         long endTimer = System.nanoTime();
