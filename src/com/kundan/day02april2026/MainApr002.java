fb7eaa8d41432ab91d442f5f7b994db4d1726c1d
kundan kumar <kundankumaracc20@gmail.com>
2026-04-02 09:45:01 +0530

    April:02::2026::09:45:AM

--- src/com/kundan/day02april2026/MainApr002.java
+++ src/com/kundan/day02april2026/MainApr002.java
@@ -1,10 +1,33 @@
 package com.kundan.day02april2026;
 
+import java.util.Stack;
+
 public class MainApr002 {
+    public boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c=='(')
+                stack.push(')');
+            else if (c=='{')
+                stack.push('}');
+            else if (c=='[')
+                stack.push(']');
+            else {
+                if (stack.isEmpty() || stack.pop() !=c)
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
-
+        MainApr002 vp = new MainApr002();
+        System.out.println(vp.isValid("()"));        // true
+        System.out.println(vp.isValid("()[]{}"));    // true
+        System.out.println(vp.isValid("(]"));        // false
+        System.out.println(vp.isValid("([)]"));      // false
+        System.out.println(vp.isValid("{[]}"));      // true
         //end timer
         long endTimer = System.nanoTime();
 
