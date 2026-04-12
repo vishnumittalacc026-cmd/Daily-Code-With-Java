dd8b841696c90ca5398982a99d5e23268a8fa230
kundan kumar <kundankumaracc20@gmail.com>
2026-04-12 14:09:02 +0530

    April:12::2026::14:09:PM

--- src/com/kundan/day12april2026/MainApr02.java
+++ src/com/kundan/day12april2026/MainApr02.java
@@ -1,10 +1,38 @@
 package com.kundan.day12april2026;
 
 public class MainApr02 {
+    public static Node insert(Node head, int data){
+        Node newNode = new Node(data);
+        if(head == null)
+            return newNode;
+
+        Node temp =head;
+        while (temp.next != null){
+            temp= temp.next;
+        }
+        temp.next = newNode;
+        return head;
+    }
+    public static void printList(Node head){
+        Node temp = head;
+        while (temp != null){
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
     public static void main(String[] args){
         // start timer
         long startTimer = System.nanoTime();
+        Node head =null;
 
+        head = insert(head,10);
+        head = insert(head, 11);
+        head = insert(head, 12);
+        head = insert(head, 14);
+
+        System.out.println("Linked list: ");
+        printList(head);
         //end timer
         long endTimer = System.nanoTime();
 
