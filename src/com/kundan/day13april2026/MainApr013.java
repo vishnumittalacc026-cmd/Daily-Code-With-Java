61ae5c0bf48041290101e1f6b2ad8bfab184d189
kundan kumar <kundankumaracc20@gmail.com>
2026-04-13 09:29:02 +0530

    April:13::2026::09:29:AM

--- src/com/kundan/day13april2026/MainApr013.java
+++ src/com/kundan/day13april2026/MainApr013.java
@@ -1,15 +1,33 @@
 package com.kundan.day13april2026;
 
 public class MainApr013 {
+    //search method
+     static boolean search(Node head, int key){
+        for (Node curr = head; curr != null; curr =curr.next){
+            if(curr.data==key)
+                return true;
+        }
+        return false;
+    }
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        Node head = new Node(10);
+        head.next = new Node(11);
+        head.next.next= new Node(12);
+        head.next.next.next = new Node(7);
+
+        int key1 =11;
+        int key2 = 12;
+
+        System.out.println("is "+ key1 + " present ?"+search(head,key1));
+        System.out.println("is "+ key2 + " present ?"+search(head,key2));
 
         //end timer
         long endTimer = System.nanoTime();
 
         //program time
-        long programTime = System.nanoTime();
+        long programTime = (endTimer-startTimer)/1_000_000;
 
         System.out.println("Program time: "+programTime+" ms");
     }
