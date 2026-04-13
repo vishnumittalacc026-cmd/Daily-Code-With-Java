bb2a49178ac9a1b9c5493573e526be0545af619c
kundan kumar <kundankumaracc20@gmail.com>
2026-04-13 09:20:08 +0530

    April:13::2026::09:20:AM

---
+++ src/com/kundan/day13april2026/MainApr013.java
@@ -0,0 +1,16 @@
+package com.kundan.day13april2026;
+
+public class MainApr013 {
+    public static void main(String[] args){
+        //start timer
+        long startTimer = System.nanoTime();
+
+        //end timer
+        long endTimer = System.nanoTime();
+
+        //program time
+        long programTime = System.nanoTime();
+
+        System.out.println("Program time: "+programTime+" ms");
+    }
+}
