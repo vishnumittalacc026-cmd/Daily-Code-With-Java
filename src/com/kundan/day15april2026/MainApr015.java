0fc0ec89c36d5b0effd26cae5592843d7e329624
kundan kumar <kundankumaracc20@gmail.com>
2026-04-15 09:33:02 +0530

    April:15::2026::09:33:AM

--- src/com/kundan/day15april2026/MainApr015.java
+++ src/com/kundan/day15april2026/MainApr015.java
@@ -1,10 +1,39 @@
 package com.kundan.day15april2026;
 
 public class MainApr015 {
+    //recursive method
+    static Node reverse(Node head){
+        if(head==null || head.next==null)
+            return head;
+        Node newHead = reverse(head.next);
+        head.next.next = head;
+        head.next =null;
+        return newHead;
+    }
+    //Utility method
+    static void printList(Node head){
+        Node current = head;
+        while (current != null){
+            System.out.print(current.data+" ");
+            current = current.next;
+        }
+        System.out.println();
+    }
     public static void main(String[] args){
         // start time
         long startTimer = System.nanoTime();
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next= new Node(4);
+        head.next.next.next.next = new Node(5);
 
+        System.out.println("original List: ");
+        printList(head);
+
+        head =reverse(head);
+        System.out.println("Reversed List: ");
+        printList(head);
         //end time
         long endTimer = System.nanoTime();
 
