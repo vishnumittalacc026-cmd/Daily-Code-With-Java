07fcd7c296afeec17edf7efe5e0635785a0acc5d
kundan kumar <kundankumaracc20@gmail.com>
2026-04-16 09:51:17 +0530

    April:16::2026::09:51:AM

--- src/com/kundan/day16april2026/Main.java
+++ src/com/kundan/day16april2026/Main.java
@@ -1,10 +1,36 @@
 package com.kundan.day16april2026;
 
 public class Main {
+    public static Node middle(Node head){
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public static void printLinkedList(Node head){
+        Node newNode = head;
+        while (newNode != null){
+            System.out.print(newNode.data + "->");
+            newNode = newNode.next;
+        }
+        System.out.println("null");
+    }
     public static void main(String[] args){
         // start timer
         long startTimer = System.nanoTime();
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
 
+        System.out.println("Linked List: ");
+        printLinkedList(head);
+
+        Node mid = middle(head);
+        System.out.println("Middle Node: "+ mid.data);
         //end timer
         long endTimer = System.nanoTime();
 
