55aee73200fc898aafb979c3622cb295be091791
kundan kumar <kundankumaracc20@gmail.com>
2026-04-17 10:12:01 +0530

    April:17::2026::10:12:AM

--- src/com/kundan/day17april2026/Main.java
+++ src/com/kundan/day17april2026/Main.java
@@ -4,6 +4,16 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        Circle circle = new Circle(5.0);
+        Rectangle rectangle = new Rectangle(10.0, 4.0);
+        Triangle triangle = new Triangle(6.0, 3.0);
+        RightTriangle rightTriangle = new RightTriangle(8.0, 4.0);
+
+        //print details
+        System.out.println("Circle with radius: "+ circle.radius);
+        System.out.println("Rectangle with length: "+rectangle.length+" and width: "+rectangle.width);
+        System.out.println("Triangle with base: "+ triangle.base+ " and base "+ triangle.base);
+        System.out.println("RightTriangle with base: "+ rightTriangle.base+" height: "+ rightTriangle.height);
 
         //end timer
         long endTimer = System.nanoTime();
