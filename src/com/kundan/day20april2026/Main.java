d198b926a9305c7edec695267a7aeba3dfbd7668
kundan kumar <kundankumaracc20@gmail.com>
2026-04-20 09:12:01 +0530

    April:20::2026::09:12:AM

--- src/com/kundan/day20april2026/Main.java
+++ src/com/kundan/day20april2026/Main.java
@@ -4,13 +4,25 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        LinearQueue q = new LinearQueue(5);
 
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+        q.enqueue(5);
+
+        q.display();
+
+        q.dequeue();
+        q.display();
+        System.out.println("front element is: "+q.peek());
         //end Timer
         long endTimer = System.nanoTime();
 
         //program time
         long programTime = (endTimer-startTimer)/1_000_000;
 
-        System.out.println(programTime);
+        System.out.println("program time: " +programTime+" ms");
     }
 }
