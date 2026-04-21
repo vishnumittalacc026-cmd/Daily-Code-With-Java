524cad7c05230dedecdad5b66dd66e151f34ee59
kundan kumar <kundankumaracc20@gmail.com>
2026-04-21 09:40:07 +0530

    April:21::2026::09:40:AM

--- src/com/kundan/day21april2026/LinkedListQueue.java
+++ src/com/kundan/day21april2026/LinkedListQueue.java
@@ -1,4 +1,45 @@
 package com.kundan.day21april2026;
 
 public class LinkedListQueue {
+    Node front, rear;
+
+    void enqueue(int data){
+        Node newNode = new Node(data);
+
+        if(rear == null){
+            front = rear = newNode;
+            return;
+        }
+        rear.next = newNode;
+        rear = newNode;
+    }
+
+    int dequeue(){
+        if(front == null){
+            System.out.println("Queue is Empty");
+            return -1;
+        }
+        int value = front.data;
+        front = front.next;
+
+        if(front == null)
+            rear = null;
+        return value;
+    }
+    boolean isEmpty(){
+        return front==null;
+    }
+
+    void display(){
+        if(front == null){
+            System.out.println("queue is empty");
+            return;
+        }
+        Node temp = front;
+        while (temp != null){
+            System.out.println(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
 }
