2f6bea7ad3008a99d5189e6f83902af3650617cf
kundan kumar <kundankumaracc20@gmail.com>
2026-04-21 09:48:04 +0530

    April:21::2026::09:48:AM

--- src/com/kundan/day21april2026/Main.java
+++ src/com/kundan/day21april2026/Main.java
@@ -4,7 +4,24 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        LinkedListQueue queue = new  LinkedListQueue();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
 
+        System.out.print("queue after enqueues: ");
+        queue.display();
+
+        System.out.println("Dequeue: "+queue.dequeue());
+        System.out.println("Dequeue: "+queue.dequeue());
+
+        System.out.print("queue after dequeue: ");
+        queue.display();
+        queue.enqueue(6);
+        System.out.print("Queue after enqueue 6: ");
+        queue.display();
         //end timer
         long endTimer = System.nanoTime();
 
