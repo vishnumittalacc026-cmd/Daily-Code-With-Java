90834dc3c96acc3550f9fb9556e0838a85932e13
kundan kumar <kundankumaracc20@gmail.com>
2026-04-22 09:22:02 +0530

    April:22::2026::09:22:AM

--- src/com/kundan/day22april2026/Main.java
+++ src/com/kundan/day22april2026/Main.java
@@ -1,10 +1,23 @@
 package com.kundan.day22april2026;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        //using java build-in Queue interface
+        Queue<Integer> queue = new LinkedList<>();
 
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+
+        System.out.println(queue);
+
+        System.out.println(queue.remove());
+        System.out.println(queue.peek());
         //end timer
         long endTimer = System.nanoTime();
 
