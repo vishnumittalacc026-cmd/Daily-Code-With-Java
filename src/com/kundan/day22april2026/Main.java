c82259870019c42a7f38d8dddc6bd98ccc18c713
kundan kumar <kundankumaracc20@gmail.com>
2026-04-22 09:28:01 +0530

    April:22::2026::09:28:AM

--- src/com/kundan/day22april2026/Main.java
+++ src/com/kundan/day22april2026/Main.java
@@ -1,23 +1,36 @@
 package com.kundan.day22april2026;
 
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
-        //using java build-in Queue interface
-        Queue<Integer> queue = new LinkedList<>();
+//        //using java build-in Queue interface
+//        Queue<Integer> queue = new LinkedList<>();
+//
+//        queue.add(10);
+//        queue.add(20);
+//        queue.add(30);
+//
+//        System.out.println(queue);
+//
+//        System.out.println(queue.remove());
+//        System.out.println(queue.peek());
 
-        queue.add(10);
-        queue.add(20);
-        queue.add(30);
+        //PriorityQueue
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        System.out.println(queue);
+        pq.add(3);
+        pq.add(1);
+        pq.add(2);
 
-        System.out.println(queue.remove());
-        System.out.println(queue.peek());
+        while (!pq.isEmpty()){
+            System.out.print(pq.poll()+" ");
+        }
+        System.out.println();
         //end timer
         long endTimer = System.nanoTime();
 
