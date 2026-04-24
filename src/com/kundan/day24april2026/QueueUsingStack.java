14fc59272f8ab8c4936318dea3ce55d6d8541131
kundan kumar <kundankumaracc20@gmail.com>
2026-04-24 08:59:14 +0530

    April:24::2026::08:59:AM

---
+++ src/com/kundan/day24april2026/QueueUsingStack.java
@@ -0,0 +1,27 @@
+package com.kundan.day24april2026;
+
+import java.util.Stack;
+
+public class QueueUsingStack {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    void enqueue(int x){
+        s1.push(x);
+    }
+    int dequeue(){
+        if(s1.isEmpty() && s2.isEmpty()) {
+            System.out.println("queue is empty");
+            return -1;
+        }
+        if(s2.isEmpty()){
+            while (!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
+    }
+    boolean isEmpty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+}
