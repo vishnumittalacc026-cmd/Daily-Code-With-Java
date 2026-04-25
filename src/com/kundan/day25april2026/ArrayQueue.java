1efed433dd83c9e2f0185006557606bd653f81b4
kundan kumar <kundankumaracc20@gmail.com>
2026-04-25 12:04:46 +0530

    April:25::2026::12:04:PM

--- src/com/kundan/day25april2026/ArrayQueue.java
+++ src/com/kundan/day25april2026/ArrayQueue.java
@@ -1,4 +1,27 @@
 package com.kundan.day25april2026;
 
 public class ArrayQueue {
+    int[] arr;
+    int front =0, rear = -1, size =0, capacity;
+
+    ArrayQueue(int capacity){
+        this.capacity = capacity;
+        arr = new int[capacity];
+    }
+
+    void enqueue(int x){
+        if(size == capacity)
+            return;
+        rear = (rear+1) % capacity;
+        arr[rear] = x;
+        size++;
+    }
+    int deque(){
+        if(size == 0)
+            return -1;
+        int val = arr[front];
+        front = (front+1)%capacity;
+        size--;
+        return val;
+    }
 }
