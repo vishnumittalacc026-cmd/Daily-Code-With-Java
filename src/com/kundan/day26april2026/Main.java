2d7c8adb37d157cb48a67a3fc17c6e9a8b570d33
kundan kumar <kundankumaracc20@gmail.com>
2026-04-26 17:55:20 +0530

    April:26::2026::17:55:PM

--- src/com/kundan/day26april2026/Main.java
+++ src/com/kundan/day26april2026/Main.java
@@ -1,10 +1,15 @@
 package com.kundan.day26april2026;
 
+record Point(int x, int y){}
 public class Main {
     public static void main(String[] args) {
         //start timer
         long startTimer = System.nanoTime();
+        Object obj = new Point(1,2);
 
+        if(obj instanceof  Point(int x, int y)){
+            System.out.println("Coordinates: "+x+", "+y);
+        }
         //end timer
         long endTimer = System.nanoTime();
 
