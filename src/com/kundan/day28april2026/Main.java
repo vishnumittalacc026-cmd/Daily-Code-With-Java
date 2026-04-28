0dfee4958c94c0a3ee2d4982491bfc5136dada11
kundan kumar <kundankumaracc20@gmail.com>
2026-04-28 09:06:01 +0530

    Arpil:28::2026::09:06:AM

--- src/com/kundan/day28april2026/Main.java
+++ src/com/kundan/day28april2026/Main.java
@@ -1,10 +1,27 @@
 package com.kundan.day28april2026;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        Queue<Integer> q = new LinkedList<>();
 
+        // adding element to the queue
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        System.out.println("Original Queue: "+q);
+
+        //reverse the queue
+        ReverseQueue.reverse(q);
+
+        System.out.println("Reversed Queue: "+q);
         //end timer
         long endTimer = System.nanoTime();
 
--- src/com/kundan/day28april2026/ReverseQueue.java
+++ src/com/kundan/day28april2026/ReverseQueue.java
@@ -1,4 +1,13 @@
 package com.kundan.day28april2026;
 
+import java.util.Queue;
+
 public class ReverseQueue {
+    static void reverse(Queue<Integer> q){
+        if(q.isEmpty())
+            return;
+        int x = q.poll();
+        reverse(q);
+        q.add(x);
+    }
 }
