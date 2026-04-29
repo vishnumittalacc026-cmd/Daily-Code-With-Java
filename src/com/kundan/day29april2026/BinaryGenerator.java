6aa34755d6055486536f4091c9da51961a4c0cca
kundan kumar <kundankumaracc20@gmail.com>
2026-04-29 08:58:01 +0530

    April:29::2026::08:58:AM

--- src/com/kundan/day29april2026/BinaryGenerator.java
+++ src/com/kundan/day29april2026/BinaryGenerator.java
@@ -1,4 +1,19 @@
 package com.kundan.day29april2026;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryGenerator {
+    static void generate(int n){
+        Queue<String> q = new LinkedList<>();
+
+        q.add("1");
+
+        for(int i=0; i<n; i++){
+            String curr = q.poll();
+            System.out.println(curr+ " ");
+            q.add(curr+"0");
+            q.add(curr+"1");
+        }
+    }
 }
