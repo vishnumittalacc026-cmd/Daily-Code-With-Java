d30d129fa7a59b04ff7e26973a6c379901125d9f
kundan kumar <kundankumaracc20@gmail.com>
2026-04-30 09:06:14 +0530

    April:30::2026::09:06:AM

--- src/com/kundan/day30april2026/Main.java
+++ src/com/kundan/day30april2026/Main.java
@@ -4,7 +4,18 @@ public class Main {
    public static void main(String[] args){
        //start timer
        long startTimer = System.nanoTime();
+        QueueUsingStacks q = new QueueUsingStacks();
 
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+
+        System.out.println("Front element: "+q.peek());
+        System.out.println("Dequeued: "+q.dequeue());
+        System.out.println("Dequeued: "+q.dequeue());
+        System.out.println("is queue empty? "+q.isEmpty());
+        System.out.println("Dequeued: "+q.dequeue());
+        System.out.println("is queue empty? "+q.isEmpty());
        //end timer
        long endTimer = System.nanoTime();
 
