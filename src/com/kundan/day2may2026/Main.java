62f841a43346bb095f11c62266b4564d28ed1ab9
kundan kumar <kundankumaracc20@gmail.com>
2026-05-02 21:14:44 +0530

    May:02::2026::21:14:PM

--- src/com/kundan/day2may2026/Main.java
+++ src/com/kundan/day2may2026/Main.java
@@ -1,10 +1,33 @@
 package com.kundan.day2may2026;
 
+import java.util.ArrayList;
+import java.util.SequencedCollection;
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        SequencedCollection<String> names = new ArrayList<>();
 
+        names.add("Alice");
+        names.add("bob");
+        names.add("charlie");
+
+        System.out.println("First: "+names.getFirst());
+        System.out.println("Last: "+names.getLast());
+
+        names.addFirst("zara");
+        names.addLast("david");
+
+        System.out.println("After adding at ends: "+names);
+
+        names.removeFirst();
+        names.removeLast();
+
+        System.out.println("after removing end: "+names);
+
+        SequencedCollection<String> reversedNames = names.reversed();
+        System.out.println("Reversed view: "+reversedNames);
         //end timer
         long endTimer = System.nanoTime();
 
