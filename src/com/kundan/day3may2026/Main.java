5cb1ef63d01b1592f325616067be5927a7dd4f62
kundan kumar <kundankumaracc20@gmail.com>
2026-05-03 15:36:04 +0530

    May:03::2026::15:36:PM

--- src/com/kundan/day3may2026/Main.java
+++ src/com/kundan/day3may2026/Main.java
@@ -1,10 +1,23 @@
 package com.kundan.day3may2026;
 
 public class Main {
+    public static double getPerimeter(Shape s){
+        if(s instanceof Rectangle r){
+            return 2* r.length()+ 2*r.width();
+        } else if(s instanceof Circle c){
+            return 2*c.radius()*Math.PI;
+        } else {
+            throw new IllegalArgumentException("Unrecognized shape");
+        }
+    }
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        Shape rect = new Rectangle(10,5);
+        Shape circle = new Circle(7);
 
+        System.out.println("Rectangle perimeter: "+getPerimeter(rect));
+        System.out.println("Circle perimeter: "+getPerimeter(circle));
         //end timer
         long endTimer = System.nanoTime();
 
