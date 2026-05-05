7b9c9fa29fb2883122ac922c83d5d6e19cfc8834
kundan kumar <kundankumaracc20@gmail.com>
2026-05-05 08:49:02 +0530

    May:05::2026::08:49:AM

--- src/com/kundan/day05may2026/Main.java
+++ src/com/kundan/day05may2026/Main.java
@@ -1,10 +1,17 @@
 package com.kundan.day05may2026;
 
 public class Main {
+    public static int factorial(int n){
+        if(n==0){
+            return 1;
+        }
+        return n*factorial(n-1);
+    }
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
-
+        int result = factorial(5);
+        System.out.println("Factorial: "+result);
         //end timer
         long endTimer = System.nanoTime();
 
