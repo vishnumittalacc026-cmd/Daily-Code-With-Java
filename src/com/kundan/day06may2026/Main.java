fe8cc8817fcafc638d44a7ca5f0ebdf69dd885dc
kundan kumar <kundankumaracc20@gmail.com>
2026-05-06 09:05:01 +0530

    May:06::2026::09:05:AM

--- src/com/kundan/day06may2026/Main.java
+++ src/com/kundan/day06may2026/Main.java
@@ -1,10 +1,25 @@
 package com.kundan.day06may2026;
 
+import java.util.Scanner;
+
 public class Main {
+    public static int fibonacci(int n){
+        if(n <= 1){
+            return n;
+        }
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter how many Fibonacci number to display: ");
+        int count = scanner.nextInt();
         //start timer
         long startTimer = System.nanoTime();
-
+        System.out.println("Fibonacci sequence up to "+count+" term: ");
+        for(int i =0; i<count; i++){
+            System.out.print(fibonacci(i)+ " ");
+        }
+        scanner.close();
         //end timer
         long endTimer = System.nanoTime();
 
