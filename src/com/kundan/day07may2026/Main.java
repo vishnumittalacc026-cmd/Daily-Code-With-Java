dae5a3dbde74469a80d5508733d087e358d48f32
kundan kumar <kundankumaracc20@gmail.com>
2026-05-07 08:52:22 +0530

    May:07::2026::08:52:AM

--- src/com/kundan/day07may2026/Main.java
+++ src/com/kundan/day07may2026/Main.java
@@ -1,16 +1,14 @@
 package com.kundan.day07may2026;
 
 public class Main {
-    public static void print(int n){
-        {
-            if (n == 0)
-                return;
-
-
-            print(n - 1);
-            System.out.print(n + " ");
-        }
-    }
+   public static void print(int n){
+       //base condition
+       if(n==0){
+           return;
+       }
+       print(n-1);
+       System.out.print(n+ " ");
+   }
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
@@ -22,6 +20,6 @@ public class Main {
         //program time
         long programTime = (endTimer-startTimer)/1_000_000;
 
-        System.out.println("Program Time: "+programTime+" ms");
+        System.out.println("Program Time: "+programTime+" millisecond");
     }
 }
