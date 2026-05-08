ab1c8f71fb43a7f4c24c820f61364309cd810a3e
kundan kumar <kundankumaracc20@gmail.com>
2026-05-08 09:41:04 +0530

    May:08::2026::09:41:AM

--- src/com/kundan/day08may2026/Main.java
+++ src/com/kundan/day08may2026/Main.java
@@ -1,18 +1,86 @@
 package com.kundan.day08may2026;
 
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
 
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+//        try {
+//            //get local host
+//            InetAddress ip = InetAddress.getLocalHost();
+//
+//            System.out.println("Ip address: "+ ip.getHostAddress());
+//
+//            System.out.println("Host name: "+ip.getHostName());
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        }
+
         try {
-            //get local host
-            InetAddress ip = InetAddress.getLocalHost();
+//            //get local host
+//            InetAddress ip = InetAddress.getLocalHost();
+//
+//            //get network interface
+//            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+//
+//            //get MAC address in byte format
+//            byte[] mac = network.getHardwareAddress();
+//
+//            //convert mac address byte to readable format
+//            StringBuilder sb = new StringBuilder();
+//
+//            for(int i=0; i< mac.length; i++){
+//                sb.append(String.format("%02X%s",
+//                        mac[i],
+//                        (i<mac.length-1)? "-":""));
+//            }
+//            //print details
+//            System.out.println("Ip Address: "+ip.getHostAddress());
+//            System.out.println("Mac Address: "+sb.toString());
 
-            System.out.println("Ip address: "+ ip.getHostAddress());
 
-            System.out.println("Host name: "+ip.getHostName());
+            Enumeration<NetworkInterface> networks = NetworkInterface.getNetworkInterfaces();
+
+            while (networks.hasMoreElements()){
+                NetworkInterface network = networks.nextElement();
+
+                //skip invalid interface
+                if(network.isLoopback()
+                || !network.isUp()
+                || network.isVirtual()){
+                    continue;
+                }
+
+                byte[] mac = network.getHardwareAddress();
+
+                if(mac == null){
+                    continue;
+                }
+
+                StringBuilder sb = new StringBuilder();
+
+                for(int i =0; i<mac.length;i++){
+                    sb.append(String.format("%02X%s",
+                            mac[i],
+                            (i<mac.length-1)?"-":""));
+                }
+
+                System.out.println("Interface name: "+network.getDisplayName());
+                System.out.println("MAC address: "+sb);
+
+                //print ip address
+                Enumeration<InetAddress> addresses = network.getInetAddresses();
+
+                while (addresses.hasMoreElements()){
+                    InetAddress addr = addresses.nextElement();
+
+                    System.out.println("IP Address: "+addr.getHostAddress());
+                }
+                System.out.println("-------------------------------");
+            }
         } catch (Exception e){
             e.printStackTrace();
         }
