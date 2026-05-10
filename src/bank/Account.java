cb1af9b0b6221f5d24e6778b85b29f492932e2f0
kundan kumar <kundankumaracc20@gmail.com>
2026-05-10 11:56:01 +0530

    May:10::2026::11:56:AM

---
+++ src/bank/Account.java
@@ -0,0 +1,46 @@
+package bank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account {
+    private final int accountNumber;
+    private final String holderName;
+    private double balance;
+    private final List<Transaction> transactions = new ArrayList<>();
+
+    public Account(int accountNumber, String holderName, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        transactions.add(new Transaction(TransactionType.DEPOSIT, amount));
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            throw new RuntimeException("Insufficient Balance!");
+        }
+        balance -= amount;
+        transactions.add(new Tranaction(TransactionType.WITHDRAW, amount));
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+}
