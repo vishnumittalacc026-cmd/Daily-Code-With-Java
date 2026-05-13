a6d7b0bf74dcf00b05292a0919047b10416e1e3f
kundan kumar <kundankumaracc20@gmail.com>
2026-05-13 09:39:32 +0530

    May:13::2026::09:39:AM

--- src/com/kundan/day13may2026/Main.java
+++ src/com/kundan/day13may2026/Main.java
@@ -4,6 +4,17 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left= new Node(4);
+        root.left.right= new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println("Tree created");
 
         //end timer
         long endTimer = System.nanoTime();
