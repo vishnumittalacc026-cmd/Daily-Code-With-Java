dc42ed534fa6554587f4a691a3274d94caa58799
kundan kumar <kundankumaracc20@gmail.com>
2026-05-14 09:30:02 +0530

    May:14::2026::09:30:AM

--- src/com/kundan/day14may2026/LevelOrderTraversal.java
+++ src/com/kundan/day14may2026/LevelOrderTraversal.java
@@ -1,4 +1,28 @@
 package com.kundan.day14may2026;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class LevelOrderTraversal {
+    public void levelOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            System.out.print(current.data + " ");
+
+            if (current.left != null) {
+                queue.add(current.left);
+            }
+
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+        }
+    }
 }
