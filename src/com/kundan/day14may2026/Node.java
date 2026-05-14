611afe1b0aea6e06b9b0c551c4ec1a2b717f74c6
kundan kumar <kundankumaracc20@gmail.com>
2026-05-14 09:22:44 +0530

    May:14::2026::09:22:AM

--- src/com/kundan/day14may2026/Node.java
+++ src/com/kundan/day14may2026/Node.java
@@ -1,4 +1,13 @@
 package com.kundan.day14may2026;
 
 public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
 }
