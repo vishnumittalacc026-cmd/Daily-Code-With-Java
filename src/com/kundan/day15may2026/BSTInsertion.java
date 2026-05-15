5a079ec8092d332f02bf8f5ae5f44158d9dbcce3
kundan kumar <kundankumaracc20@gmail.com>
2026-05-15 09:23:01 +0530

    May:15::2026::09:23:AM

--- src/com/kundan/day15may2026/BSTInsertion.java
+++ src/com/kundan/day15may2026/BSTInsertion.java
@@ -1,4 +1,28 @@
 package com.kundan.day15may2026;
 
 public class BSTInsertion {
+    //BST insertion
+    public Node insert(Node root, int value) {
+        if (root == null) {
+            return new Node(value);
+        }
+
+        if (value < root.data) {
+            root.left = insert(root.left, value);
+        } else if (value > root.data) {
+            root.right = insert(root.right, value);
+        }
+
+        return root;
+    }
+        //Inorder traversal
+        public void inorder(Node root){
+            if(root == null){
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data+" ");
+            inorder(root.right);
+
+    }
 }
