b8b8225495466604d841620676452fc98d0a0f29
kundan kumar <kundankumaracc20@gmail.com>
2026-05-18 07:18:02 +0530

    May:18::2026::07:18:AM

--- src/com/kundan/day18may2026/Main.java
+++ src/com/kundan/day18may2026/Main.java
@@ -4,7 +4,19 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        Node root = new Node(11);
 
+        root.left = new Node(12);
+        root.right = new Node(13);
+
+        root.left.left = new Node(15);
+        root.left.right = new Node(14);
+
+        root.right.left = new Node(16);
+        root.right.right = new Node(17);
+
+        int totalNodes = BinaryTreeCount.count(root);
+        System.out.println("Total number of nodes: "+ totalNodes);
         //end timer
         long endTimer = System.nanoTime();
 
