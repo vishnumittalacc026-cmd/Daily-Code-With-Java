fc5c9b2caff96999e8c0e8a96446ab773423568e
kundan kumar <kundankumaracc20@gmail.com>
2026-05-23 16:20:56 +0530

    May:23:2026::16:20:PM

--- src/com/kundan/day23may2026/TreeInorderTraversal.java
+++ src/com/kundan/day23may2026/TreeInorderTraversal.java
@@ -1,4 +1,9 @@
 package com.kundan.day23may2026;
 
 public class TreeInorderTraversal {
+    public static void inorderTraversal(Node root){
+        inorderTraversal(root.left);
+        System.out.print(root.data+" ");
+        inorderTraversal(root.right);
+    }
 }
