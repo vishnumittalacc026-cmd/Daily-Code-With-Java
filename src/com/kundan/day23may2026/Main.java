1e0498dd7f3008c45d0adf1cae5a29bd5354f856
kundan kumar <kundankumaracc20@gmail.com>
2026-05-23 16:33:03 +0530

    May:23::2026::16:33:PM

--- src/com/kundan/day23may2026/Main.java
+++ src/com/kundan/day23may2026/Main.java
@@ -4,7 +4,19 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        Node root = new Node(1);
+        root.left = new Node(4);
+        root.right = new Node(2);
 
+        root.left.left = new Node(8);
+        root.left.right = new Node(7);
+
+        root.right.left = new Node(3);
+        root.right.right = new Node(5);
+
+        System.out.println("Inorder Traversal: ");
+        TreeInorderTraversal.inorderTraversal(root);
+        System.out.println();
         //end timer
         long endTimer = System.nanoTime();
 
--- src/com/kundan/day23may2026/TreeInorderTraversal.java
+++ src/com/kundan/day23may2026/TreeInorderTraversal.java
@@ -2,6 +2,8 @@ package com.kundan.day23may2026;
 
 public class TreeInorderTraversal {
     public static void inorderTraversal(Node root){
+        if(root == null)
+            return;
         inorderTraversal(root.left);
         System.out.print(root.data+" ");
         inorderTraversal(root.right);
