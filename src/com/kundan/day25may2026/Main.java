300d078f4b402a20249e0b7866b8ef464ad3019b
kundan kumar <kundankumaracc20@gmail.com>
2026-05-25 21:03:06 +0530

    May:25::2026::21:03:PM

--- src/com/kundan/day25may2026/Main.java
+++ src/com/kundan/day25may2026/Main.java
@@ -4,7 +4,16 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        LRUCache cache = new LRUCache(2);
 
+        cache.put(1, 10);
+        cache.put(2, 20);
+
+        System.out.println(cache.get(1));
+
+        cache.put(3,30);
+
+        System.out.println(cache.get(2));
         //end timer
         long endTimer = System.nanoTime();
 
