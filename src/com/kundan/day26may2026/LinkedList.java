6aadab507cc0848fa4d759f93bc0cb60cd2396b0
kundan kumar <kundankumaracc20@gmail.com>
2026-05-26 06:52:54 +0530

    May:26::2026::06:52:AM

---
+++ src/com/kundan/day26may2026/LinkedList.java
@@ -0,0 +1,4 @@
+package com.kundan.day26may2026;
+
+public class LinkedList {
+}
---
+++ src/com/kundan/day26may2026/Node.java
@@ -0,0 +1,11 @@
+package com.kundan.day26may2026;
+
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int val){
+        data = val;
+        next =null;
+    }
+}
