0d355da8affa9b70002d4299e044a96cec4999ea
kundan kumar <kundankumaracc20@gmail.com>
2026-05-26 07:04:04 +0530

    May:26::2026::07::04:AM

--- src/com/kundan/day26may2026/Main.java
+++ src/com/kundan/day26may2026/Main.java
@@ -4,7 +4,13 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        LinkedList list = new LinkedList();
+        list.insertAtEnd(5);
+        list.insertAtEnd(2);
+        list.insertAtEnd(1);
+        list.insertAtEnd(6);
 
+        list.display();
         //end timer
         long endTimer = System.nanoTime();
 
