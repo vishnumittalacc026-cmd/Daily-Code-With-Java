f4ed515fcc687cce3246b6d4eaa9281d2508ec1b
kundan kumar <kundankumaracc20@gmail.com>
2026-05-27 07:19:11 +0530

    May:27::2026::07:19:AM

--- src/com/kundan/day27may2026/LinkedList.java
+++ src/com/kundan/day27may2026/LinkedList.java
@@ -1,4 +1,29 @@
 package com.kundan.day27may2026;
 
 public class LinkedList {
+    private Node head;
+
+    //Insert at the end
+    public void insert(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    //Display the list
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+ " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
 }
