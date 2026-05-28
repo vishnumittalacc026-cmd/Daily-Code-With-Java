4a369bf5999aa72ff3d4816854243b895ad21fb7
kundan kumar <kundankumaracc20@gmail.com>
2026-05-28 07:06:33 +0530

    May:28:2026::07:06:AM

--- src/com/kundan/day28may2026/Main.java
+++ src/com/kundan/day28may2026/Main.java
@@ -4,12 +4,28 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        //old way - platform thread
+        Thread platformThread = new Thread(() -> {
+            System.out.println("running on : "+Thread.currentThread());
+        });
+        platformThread.start();
 
+        //New way - virtual thread(java 21)
+        Thread virtualThread = Thread.ofVirtual().start(()->{
+            System.out.println("This java java 21 thread running on: "+Thread.currentThread());
+        });
+        //wait for both thread to finish
+        try {
+            platformThread.join();
+            virtualThread.join();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
         //end timer
         long endTimer = System.nanoTime();
 
         //program time
-        long programTime = System.nanoTime();
+        long programTime = (endTimer-startTimer)/1_000_000;
 
         System.out.println("Program Time: "+programTime+" ms");
     }
