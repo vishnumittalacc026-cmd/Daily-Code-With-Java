bb8217ae2bdd7bbc4fb64751201bc45c8a0a07a6
kundan kumar <kundankumaracc20@gmail.com>
2026-05-28 06:54:13 +0530

    May:28::2026::06:54:AM

---
+++ src/com/kundan/day28may2026/Main.java
@@ -0,0 +1,16 @@
+package com.kundan.day28may2026;
+
+public class Main {
+    public static void main(String[] args){
+        //start timer
+        long startTimer = System.nanoTime();
+
+        //end timer
+        long endTimer = System.nanoTime();
+
+        //program time
+        long programTime = System.nanoTime();
+
+        System.out.println("Program Time: "+programTime+" ms");
+    }
+}
