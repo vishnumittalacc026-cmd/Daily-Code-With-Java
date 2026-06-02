7a257119e1cfba02c6829a85b3a79f307359a340
kundan kumar <kundankumaracc20@gmail.com>
2026-06-02 06:58:15 +0530

    June:02::2026::06:58:AM

--- src/com/kundan/day02june2026/Main.java
+++ src/com/kundan/day02june2026/Main.java
@@ -4,7 +4,15 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        LinkedList lst = new LinkedList();
 
+        lst.insert(9);
+        lst.insert(6);
+        lst.insert(1);
+        lst.insert(2);
+        lst.insert(8);
+
+        System.out.println("No. of Nodes in the given linked list is: "+lst.count());
         //end timer
         long endTimer = System.nanoTime();
 
