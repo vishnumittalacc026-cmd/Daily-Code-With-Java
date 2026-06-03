6c20c9190afee33d0b3e6a9e2b2792e9c4401ff3
kundan kumar <kundankumaracc20@gmail.com>
2026-06-03 07:07:37 +0530

    June:03::2026::07:07:AM

--- src/com/kundan/day03june2026/LInkedList.java
+++ src/com/kundan/day03june2026/LInkedList.java
@@ -28,4 +28,19 @@ public class LInkedList {
         }
         System.out.println("EndOfLinked:");
     }
+
+    //reversed Linked List
+    public void reverse(){
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
 }
