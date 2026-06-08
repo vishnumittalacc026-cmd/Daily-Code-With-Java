ce0bd0538a30b61df1a1a3c71ab0e29ed4ce7c15
kundan kumar <kundankumaracc20@gmail.com>
2026-06-08 08:46:38 +0530

    June:08::2026::08:46:AM

--- src/com/kundan/day08june026/StackArray.java
+++ src/com/kundan/day08june026/StackArray.java
@@ -20,6 +20,24 @@ public class StackArray {
         stack[++top] = data;
         System.out.println(data+ " pushed");
     }
+
+    //pop operation
+    public int pop(){
+        if (top == -1){
+            System.out.println("Stack UnderFlow");
+            return  -1;
+        }
+        return stack[top--];
+    }
+
+    //peek operation
+    public int peek(){
+        if(top == -1){
+            return -1;
+        }
+        return stack[top];
+    }
+
     //isEmpty
     public boolean isEmpty(){
         return top == -1;
