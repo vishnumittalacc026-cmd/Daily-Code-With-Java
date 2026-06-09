3e1cf998fc7ec24bc18571fda1aa84d0ff6e188f
kundan kumar <kundankumaracc20@gmail.com>
2026-06-09 09:23:01 +0530

    June:09::2026::09:23:AM

--- src/com/kundan/day09june2026/StackLinkedList.java
+++ src/com/kundan/day09june2026/StackLinkedList.java
@@ -1,4 +1,42 @@
 package com.kundan.day09june2026;
 
 public class StackLinkedList {
+    Node top;
+
+    //insert data into the node
+    public void push(int data){
+        Node newNode = new Node(data);
+
+        newNode.next = top;
+        top = newNode;
+
+        System.out.println(data+ " pushed");
+    }
+
+    public int pop(){
+        if(top == null){
+            System.out.println("Stack empty");
+            return -1;
+        }
+        int value = top.data;
+        top = top.next;
+
+        return value;
+    }
+
+    public int peek(){
+        if(top == null){
+            return -1;
+        }
+        return top.data;
+    }
+
+    public void display(){
+        Node temp = top;
+
+        while (temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
 }
