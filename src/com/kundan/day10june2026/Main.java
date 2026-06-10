be928d4baf3f1f83a2d64afd9b6f5fa5904716bc
kundan kumar <kundankumaracc20@gmail.com>
2026-06-10 08:07:12 +0530

    June:10::2026::08:07:AM

--- src/com/kundan/day10june2026/Main.java
+++ src/com/kundan/day10june2026/Main.java
@@ -1,9 +1,24 @@
 package com.kundan.day10june2026;
 
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
       //start timer
       long startTimer = System.nanoTime();
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(11);
+        stack.push(12);
+
+        System.out.println(stack);
+
+        System.out.println("Peek: "+stack.peek());
+        System.out.println("Pop: "+ stack.pop());
+
+        System.out.println(stack);
 
       //end timer
         long endTimer = System.nanoTime();
