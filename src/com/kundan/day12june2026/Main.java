88385cc12097ce6877a4298478eb36e256e295f3
kundan kumar <kundankumaracc20@gmail.com>
2026-06-12 08:37:23 +0530

    June:12::2026::08:37:AM

--- src/com/kundan/day12june2026/Main.java
+++ src/com/kundan/day12june2026/Main.java
@@ -1,9 +1,30 @@
 package com.kundan.day12june2026;
 
+import java.util.LinkedHashSet;
+import java.util.SequencedCollection;
+
 public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        SequencedCollection<String> broserHistory = new LinkedHashSet<>();
+
+        broserHistory.addLast("google.com");
+        broserHistory.addLast("amazon.com");
+        broserHistory.addLast("fipkart.com");
+        broserHistory.addLast("github.com");
+        broserHistory.addLast("chatgpt.com");
+        broserHistory.addLast("microsoft.com");
+        broserHistory.addLast("tsmc.com");
+
+        System.out.println("History: "+broserHistory);
+
+        System.out.println("First Visited: "+broserHistory.getFirst());
+        System.out.println("Last Visited: "+broserHistory.getLast());
+
+        broserHistory.removeFirst();
+        System.out.println("After Removing Oldest: "+broserHistory);
+        System.out.println("Reverse History : "+broserHistory.reversed());
 
         //end timer
         long endTimer = System.nanoTime();
