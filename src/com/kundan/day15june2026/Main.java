93d992806bc57d797f1f700e6064edc38fc2b2ff
kundan kumar <kundankumaracc20@gmail.com>
2026-06-15 09:04:10 +0530

    June:15::2026::09:04:AM

--- src/com/kundan/day15june2026/Main.java
+++ src/com/kundan/day15june2026/Main.java
@@ -4,6 +4,13 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        Shape s1 = new Circle(5);
+        Shape s2 = new Rectangle(4,6);
+
+        System.out.println(ShapeUtils.describeShape(s1));
+        System.out.println("Area: "+ShapeUtils.area(s1));
+        System.out.println(ShapeUtils.describeShape(s2));
+        System.out.println("Area: "+ShapeUtils.area(s2));
 
         //end timer
         long endTimer = System.nanoTime();
