674cea6994895ab0ea4e07e5605c725f59a5a1f4
kundan kumar <kundankumaracc20@gmail.com>
2026-06-17 09:30:02 +0530

    June:17::2026::09:30:AM

--- src/com/kundan/day17june2026/Main.java
+++ src/com/kundan/day17june2026/Main.java
@@ -4,6 +4,16 @@ public class Main {
     public static void main(String[] args){
         //start timer
         long startTimer = System.nanoTime();
+        BalancedChecker checker = new BalancedChecker();
+
+        String str1 = "{[()]}";
+        String str2 = "{[(})]";
+        String str3 = "{[(]})";
+
+        System.out.println("String: "+str1 + " -> "+checker.isBalanced(str1));
+        System.out.println("String: "+str2 + " -> "+checker.isBalanced(str2));
+        System.out.println("String: "+str3 + " -> "+checker.isBalanced(str3));
+
 
         //end timer
         long endTimer = System.nanoTime();
