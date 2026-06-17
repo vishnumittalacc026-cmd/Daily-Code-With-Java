78bcd2feccc06c130f77893862f1c30ab38d4ac3
kundan kumar <kundankumaracc20@gmail.com>
2026-06-17 09:23:29 +0530

    June:17::2026::09:23:AM

--- src/com/kundan/day17june2026/BalancedChecker.java
+++ src/com/kundan/day17june2026/BalancedChecker.java
@@ -1,4 +1,27 @@
 package com.kundan.day17june2026;
 
+import java.util.Stack;
+
 public class BalancedChecker {
+    public boolean isBalanced(String str){
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : str.toCharArray()){
+            if(ch == '(' || ch == '{' || ch =='['){
+                stack.push(ch);
+            } else {
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.pop();
+
+                if((ch == ')' && top != '(') ||
+                        (ch =='}' && top != '{') ||
+                        (ch == ']' && top != '[')){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
 }
