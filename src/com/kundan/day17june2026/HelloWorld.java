673b572ecf57247c04fd79dba447af1155314e99
kundan kumar <kundankumaracc20@gmail.com>
2026-06-17 10:30:39 +0530

    this is hello world from eclipse

---
+++ src/com/kundan/day17june2026/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.kundan.day17june2026;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello world from ecliple from different repository to check");
+	}
+
+}
